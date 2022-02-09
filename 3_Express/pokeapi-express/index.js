9fa049bb8c6851d5b04c26198f145c5abc79e88b
davidpa7710 <davidpa7710@gmail.com>
2022-02-08 19:46:31 -0600

    new feature with postman

--- 3_Express/pokeapi-express/index.js
+++ 3_Express/pokeapi-express/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Server on Port ${port}`);
 });
@@ -9,10 +11,40 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.send("hola mundo");
 });
-app.get("", (req, res) => {
+app.get("/trainer", (req, res) => {
   const obj = {
     id: 1,
     name: "David Palacios",
+    edad: 24,
+    region: 1,
   };
   res.json(obj);
 });
+app.get("/trainer/:idTrainer/regin/idRegion", (req, res) => {
+  const { idTrainer, idRegion } = req.params
+  const obj = {
+    id: idTrainer,
+    name: "Ash Ketchum",
+    age: 10,
+    region: idRegion
+  };
+  res.json(obj);
+});
+app.get("/trainers", (req, res) => {
+  const { page, size } = req.query;
+  if (page && size) {
+    res.json({ page, size });
+  } else {
+    res.send('Parametros Obligatorios')
+  }
+});
+
+app.post('/trainer', (req,res) => {
+  const body = req.body;
+  res.json({
+    message: 'created',
+    data: body
+  });
+});
+
+
